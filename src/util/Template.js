0f8d0d0b11a8dfa81a1d97ae5fa1d10b69ab834f
Will <kode15333@gmail.com>
2021-06-01 01:51:10 +0900

    chore: add template Video Item

---
+++ src/util/Template.js
@@ -0,0 +1,39 @@
+
+export const ModalTemplate = {
+    video({title, videoId, channelTitle, publishTime}) {
+        return `
+            <section class="video-wrapper">
+              <article class="clip">
+                <div class="preview-container">
+                  <iframe
+                    width="100%"
+                    height="118"
+                    src="https://www.youtube.com/embed/${videoId}"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
+                </div>
+                <div class="content-container pt-2 px-1">
+                  <h3>${title}</h3>
+                  <div>
+                    <a
+                      href="https://www.youtube.com/channel/UC-mOekGSesms0agFntnQang"
+                      target="_blank"
+                      class="channel-name mt-1"
+                    >
+                      ${channelTitle}
+                    </a>
+                    <div class="meta">
+                      <p>${publishTime}</p>
+                    </div>
+                    <div class="d-flex justify-end">
+                      <button class="btn">⬇️ 저장</button>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </section>
+        `
+    }
+}
