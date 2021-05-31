76bd8e1c3e5cbdd4de7e86e721e4816f1ee16f53
Will <kode15333@gmail.com>
2021-06-01 01:50:53 +0900

    feat: add YouTube Search API

---
+++ src/util/API.js
@@ -0,0 +1,60 @@
+const SEARCH_URL = `https://www.googleapis.com/youtube/v3/search?key=${process.env.GOOGLE_API_KEY}&part=snippet&order=viewCount&maxResults=10&q=`
+
+const parseResponse = async response => {
+  const { status, ok} = response;
+  let data;
+  if(ok) {
+    data = await response.json();
+  } else {
+    throw new Error('Response Error' + status)
+  }
+
+  return {
+    status,
+    data
+  }
+}
+
+const request = async params => {
+  const {
+    method = 'GET',
+    url,
+    headers = {},
+    body
+  } = params;
+
+  const config = {
+    method,
+    headers: new Headers(headers)
+  }
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  try {
+    const response = await fetch(url, config);
+    return parseResponse(response);
+  } catch (error) {
+    console.error('Fetch Error' + error)
+  }
+
+}
+
+const get = async (url, headers) => {
+  const response = await request({
+    url,
+    headers,
+    method: 'GET'
+  })
+
+  return response.data
+}
+
+export const search = async (serach) => {
+  try {
+    return await get(SEARCH_URL + serach)
+  } catch (error) {
+    console.error(error)
+  }
+}
